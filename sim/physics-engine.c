b31b839e5adc70c18ee2d6b8fb67131a853b0719
BlazeNiac <CoolBeansBigBoy@gmail.com>
2026-06-06 23:27:58 -0500

    physics engine started

--- sim/physics-engine.c
+++ sim/physics-engine.c
@@ -1,6 +1,33 @@
 #include "physics-engine.h"
 
-void sim(void) {
-    // Simulate the physics engine for testing purposes
-    // In a real implementation, this would update the altitude based on the throttle and other factors
-};
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#define SDL_FLAGS SDL_INIT_VIDEO
+
+typedef struct PhysicsEngine {
+    bool running;
+} PhysicsEngine;
+
+//Assumes engine is already allocated and initialized to zero
+void start_physics_engine(PhysicsEngine *engine) {
+    bool start = SDL_Init(SDL_FLAGS);
+    if (start != 0) {
+       engine->running = true;
+    }
+    else{
+        engine->running = false;
+    }
+    
+}
+
+void stop_physics_engine(PhysicsEngine *engine) {
+    if (engine->running) {
+        SDL_Quit();
+        engine->running = false;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 int main(){
-    SDL_Init(0);
-    SDL_Quit();
+   
     return 0;
 }
